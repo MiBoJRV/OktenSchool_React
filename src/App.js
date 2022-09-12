8b999a23334b3f55eec87d3c82cca940c14110b2
mbodnar <mikhail.bodnar@avanquest.com>
2022-09-12 10:23:49 +0300

    Create new components (Posts, Post)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import Users from "./components/users/Users";
+import Posts from "./components/posts/Posts";
 
 export default function App() {
     return (
         <div>
-            <Users/>
+            <Posts/>
         </div>
     );
 }
